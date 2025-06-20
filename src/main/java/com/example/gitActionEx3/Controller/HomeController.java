8a955e2fd2fd6e4e4c2ab10a92c53bae1c1a7c59
jaiganesh707 <jaiganeshjkm@gmail.com>
2025-06-20 10:10:32 +0530

    Hello

---
+++ src/main/java/com/example/gitActionEx3/Controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.gitActionEx3.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HomeController {
+
+    @GetMapping("/")
+    public String gitActionExample(){
+        return "Hello Dom!";
+    }
+}
---
+++ src/main/java/com/example/gitActionEx3/GitActionEx3Application.java
@@ -0,0 +1,13 @@
+package com.example.gitActionEx3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionEx3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitActionEx3Application.class, args);
+	}
+
+}
